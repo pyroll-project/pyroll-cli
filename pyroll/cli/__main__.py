1738a93a12903640546939b62b9776b5fd2b27b6
Max Weiner <max.weiner@imf.tu-freiberg.de>
2022-12-19 14:31:41 +0100

    add entry point commands

--- pyroll/cli/__main__.py
+++ pyroll/cli/__main__.py
@@ -1,4 +1,10 @@
 from .program import main
+from importlib.metadata import entry_points
 
 if __name__ == "__main__":
+    commands = entry_points(group="pyroll.cli.commands")
+
+    for c in commands:
+        main.add_command(c.load())
+
     main()
